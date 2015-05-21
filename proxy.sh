75a87dc1b74a1aa1df46fb7d9567f6798734c603
Johan Philips <johan.philips@kuleuven.be>
2015-05-21 10:52:19 +0200

    proxy startup script

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start up a proxy using the name that was provided as argument
-./proxy https://gitlab.mech.kuleuven.be/rob-picknpack/pnp-reference/raw/master/lcsm.json $1 tcp://localhost:9001 tcp://*:9002 33033
+./proxy https://gitlab.mech.kuleuven.be/u0052546/sherpa-proxy/raw/master/lcsm.json $1 9999 33033
 
