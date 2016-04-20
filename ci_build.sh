28bc7bedcad5f9bd85e5512518a3bd263075d3a2
Johan Philips <johan.philips@kuleuven.be>
2016-04-20 15:44:29 +0200

    removed tmp build dir

--- ci_build.sh
+++ ci_build.sh
@@ -3,16 +3,16 @@
 set -x
 set -e
 
-mkdir tmp
-BUILD_PREFIX=$PWD/tmp
+# mkdir tmp
+# BUILD_PREFIX=$PWD/tmp
 
 CONFIG_OPTS=()
-CONFIG_OPTS+=("CFLAGS=-I${BUILD_PREFIX}/include")
-CONFIG_OPTS+=("CPPFLAGS=-I${BUILD_PREFIX}/include")
-CONFIG_OPTS+=("CXXFLAGS=-I${BUILD_PREFIX}/include")
-CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
-CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
-CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
+#CONFIG_OPTS+=("CFLAGS=-I${BUILD_PREFIX}/include")
+#CONFIG_OPTS+=("CPPFLAGS=-I${BUILD_PREFIX}/include")
+#CONFIG_OPTS+=("CXXFLAGS=-I${BUILD_PREFIX}/include")
+#CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
+#CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
+#CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
 CONFIG_OPTS+=("--with-docs=no")
 CONFIG_OPTS+=("--quiet")
 
