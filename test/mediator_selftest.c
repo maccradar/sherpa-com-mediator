df694b2b80d6fa913dfc6e793a5c9a5568508a3f
Johan Philips <johan.philips@kuleuven.be>
2016-04-21 16:40:45 +0200

    moved selftest to test directory

---
+++ test/mediator_selftest.c
@@ -0,0 +1,58 @@
+#include "mediator.h"
+
+int
+main (int argc, char *argv [])
+{
+    bool verbose;
+    if (argc == 2 && streq (argv [1], "-v"))
+        verbose = true;
+    else
+        verbose = false;
+
+    printf ("Running mediator selftests...\n");
+
+    mediator_test (verbose);
+    
+    printf ("Tests passed OK\n");
+    return 0;
+
+}
+
+// mediator self test
+
+void mediator_test (bool verbose) {
+    return 0;
+    printf (" * mediator: ");
+    if (verbose)
+        printf ("\n");
+    
+    // @selftest
+    // Create two mediators
+    json_t * config1 = load_config_file("selftest1.json");
+    assert (config1);
+    mediator_t *mediator1 = mediator_new(config1);
+    assert (mediator1);
+    
+    json_t * config2 = load_config_file("selftest2.json");
+    assert (config2);
+    mediator_t *mediator2 = mediator_new(config2);
+    assert (mediator2);
+    
+    if (verbose) {
+        zyre_set_verbose (mediator1->remote);
+        zyre_set_verbose (mediator1->local);
+        zyre_set_verbose (mediator2->remote);
+        zyre_set_verbose (mediator2->local);
+    }
+    
+    // Give them time to interconnect
+    zclock_sleep (100);
+    if (verbose) {
+        zyre_dump (mediator1->remote);
+        zyre_dump (mediator1->local);
+        zyre_dump (mediator2->remote);
+        zyre_dump (mediator2->local);
+    }
+    
+    
+}
